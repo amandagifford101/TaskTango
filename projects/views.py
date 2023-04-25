d950f0dd1ca2c4706c3dc94ef5a5be5606d656aa
Amanda <agifford887@gmail.com>
2023-04-25 09:14:14 -0700

    Feature 14 completed

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from projects.models import Project
 from django.contrib.auth.decorators import login_required
+from projects.forms import ProjectForm
 
 # Create your views here.
 
@@ -12,3 +13,21 @@ def list_projects(request):
         "projects": projects,
     }
     return render(request, "projects/list.html", context)
+
+
+@login_required
+def create_project(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            new_receipt = form.save(False)
+            new_receipt.purchaser = request.user
+            new_receipt.save()
+            return redirect("list_projects")
+    else:
+        form = ProjectForm()
+
+    context = {
+        "form": form,
+    }
+    return render(request, "projects/create.html", context)
