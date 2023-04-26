698bf4913c114258d375d8b8668c5785cdeda0d9
Amanda <agifford887@gmail.com>
2023-04-25 17:59:01 -0700

    New UI bootstrap5 is so cool

--- projects/forms.py
+++ projects/forms.py
@@ -5,8 +5,13 @@ from projects.models import Project
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
-        fields = (
+        fields = [
             "name",
             "description",
             "owner",
-        )
+        ]
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "form-control-sm"}),
+            "description": forms.TextInput(attrs={"class": "form-control-sm"}),
+            "owner": forms.Select(attrs={"class": "form-control-sm"}),
+        }
--- tracker/settings.py
+++ tracker/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "fontawesomefree",
+    "bootstrap5",
+    "widget_tweaks",
 ]
 
 MIDDLEWARE = [
